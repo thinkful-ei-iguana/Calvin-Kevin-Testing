2da8bb423520bcb044187d46dcd8570d243be403
Kevin Moore <moore.kevin.p@gmail.com>
2019-12-04 14:02:25 -0500

    added midpoint and test

--- express_testing/test/app.test.js
+++ express_testing/test/app.test.js
@@ -9,3 +9,18 @@ describe('Express App', () => {
       .expect(200, 'Hello Express!');
   });
 });
+
+describe('GET /sum', () => {
+  it('8/4 should be 2', () => {
+    return supertest(app)
+      .get('/sum')
+      .query({ a: 8, b: 4 })
+      .expect(200, '8 divided by 4 is 2');
+  });
+  it('should return 400 is \'a\' is missing', () => {
+    return supertest(app)
+      .get('/sum')
+      .query({ b: 4 })
+      .expect(400, 'Value for a is needed');
+  });
+});
---
+++ express_testing/test/generate.test.js
@@ -0,0 +1,18 @@
+const { expect } = require('chai');
+const supertest = require('supertest');
+const app = require('../app');
+
+describe('GET /generate endpoint', () => {
+  it('should generate an array of 5', () => {
+    return supertest(app)
+      .get('/generate')
+      .query({ n: 5 })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf.at.least(1);
+        expect(res.body).to.include(5);
+      });
+  });
+});
