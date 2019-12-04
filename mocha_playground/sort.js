03452961f40660c938ea316fae27eb65242970ef
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-12-04 10:21:11 -0700

    finished drill 2, despite trickery

--- mocha_playground/sort.js
+++ mocha_playground/sort.js
@@ -10,3 +10,4 @@ function sort(list) {
   }
   return list;
 }
+module.exports = sort;
--- mocha_playground/test/divide.test.js
+++ mocha_playground/test/divide.test.js
@@ -24,9 +24,7 @@ describe("Divide", () => {
     const a = -3;
     const b = -1;
     const expectedAnswer = 3;
-    console.log(expectedAnswer);
     const actualAnswer = divide(a, b);
-    console.log(actualAnswer);
     expect(actualAnswer).to.equal(expectedAnswer);
   });
 });
